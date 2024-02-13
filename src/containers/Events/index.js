b1220cd75a70284747d5fe3797b86e97ba8bde6c
CindySab <149073779+CindySab@users.noreply.github.com>
2024-02-13 15:58:20 +0100

    correction du bug du filtre

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -16,7 +16,8 @@ const EventList = () => {
   const filteredEvents = (
     (!type
       ? data?.events
-      : data?.events) || []
+      /** ajout du filtre en fonction du type */
+      : data?.events.filter((event) => event.type === type)) || []
   ).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
